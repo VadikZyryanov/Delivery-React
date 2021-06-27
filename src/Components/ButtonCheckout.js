fca62ca93ca2e8c2089acacca2b8126c1e3c8aa9
Vadik Zyryanov <vadik94_1rec@bk.ru>
2021-06-27 14:30:21 +0500

    левое меню с заказом

---
+++ src/Components/ButtonCheckout.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ButtonCheckout = styled.button`
+    background-color: #299B01;
+    display: block;
+    width: 250px;
+    height: 65px;
+    color: #fff;
+    font-size: 21px;
+    font-family: inherit;
+    border-color: transparent;
+    margin: 0 auto;
+    transition-property: color, background-color, border-color;
+    transition-duration: .3s;
+    &:hover {
+        background-color: #fff;
+        color: #299B01;
+        border-color: #299B01;
+    }
+`;
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -7,27 +7,37 @@ import bannerImg from '../image/banner.png';
 const MenuStyled = styled.main`
     background-color: #ccc;
     margin-top: 80px;
+    margin-left: 380px;
 `;
 
 const SectionMenu = styled.section`
     padding: 30px;
 `;
 
-const BannerImg = styled.img`
+const Banner = styled.div`
     height: 250px;
     width: 100%;
+    background-image: url(${bannerImg});
+    background-position: center;
+    background-size: cover;
 `;
 
-export const Menu = () => (
+export const Menu = ({ setOpenItem }) => (
     <MenuStyled>
-        <BannerImg src={bannerImg}></BannerImg>
+        <Banner/>
         <SectionMenu>
             <h2>Бургеры</h2>
-            <ListItem itemList={dbMenu.burger}></ListItem>
+            <ListItem 
+                itemList={dbMenu.burger}
+                setOpenItem={setOpenItem}
+            ></ListItem>
         </SectionMenu>
         <SectionMenu>
             <h2>Закуски / Напитки</h2>
-            <ListItem itemList={dbMenu.other}></ListItem>
+            <ListItem 
+                itemList={dbMenu.other}
+                setOpenItem={setOpenItem}
+            ></ListItem>
         </SectionMenu>
     </MenuStyled>
 );
