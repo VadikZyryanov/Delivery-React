95c12a86cf0f9104047e8eae7ef9e4c758b94896
Vadik Zyryanov <vadik94_1rec@bk.ru>
2021-08-16 23:10:19 +0500

    choices

--- src/Components/Modal/ModalItem.js
+++ src/Components/Modal/ModalItem.js
@@ -6,7 +6,9 @@ import { useCount } from '../Hooks/useCount';
 import { totalPriceItems } from '../Functions/secondaryFunction';
 import { formatCurrency } from '../Functions/secondaryFunction';
 import { Toppings } from './Toppings';
+import { Choices } from './Choices';
 import { useToppings } from '../Hooks/useToppings';
+import { useChoices } from '../Hooks/useChoices';
 
 const Overlay = styled.div`
     position: fixed;
@@ -60,6 +62,7 @@ export const ModalItem = ({ openItem, setOpenItem, orders, setOrders }) => {
 
     const counter = useCount();
     const toppings = useToppings(openItem);
+    const choices = useChoices(openItem);
 
     const closeModal = (e) => {
         if (e.target.id === 'overlay') {
@@ -71,6 +74,7 @@ export const ModalItem = ({ openItem, setOpenItem, orders, setOrders }) => {
         ...openItem,
         count: counter.count,
         topping: toppings.toppings,
+        choice: choices.choice,
     };
 
     const addToOrder = () => {
@@ -89,11 +93,15 @@ export const ModalItem = ({ openItem, setOpenItem, orders, setOrders }) => {
                     </Description>
                     <CountItem {...counter}/>
                     {openItem.toppings && <Toppings {...toppings} />}
+                    {openItem.choices && <Choices {...choices} openItem={openItem}/>}
                     <TotalPriceItem>
                         <span>Цена:</span>
                         <span>{formatCurrency(totalPriceItems(order))}</span>
                     </TotalPriceItem>
-                    <ButtonCheckout onClick={addToOrder}>Добавить</ButtonCheckout>
+                    <ButtonCheckout 
+                        onClick={addToOrder}
+                        disabled={order.choices && !order.choice}
+                        >Добавить</ButtonCheckout>
                 </Content>
             </Modal>
         </Overlay>
